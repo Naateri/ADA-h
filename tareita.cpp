93307b61a0f0bc37f76001cbe6e86089486db7b0
Renato Postigo <renato.postigo@ucsp.edu.pe>
2018-03-15 22:05:29 -0500

    Algo

---
+++ tareita.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+//using namespace std;
+
+void insert_sort(std::vector<int>& vec){
+	int cur_value, j;
+	for(int i = 0; i < vec.size(); i++){
+		cur_value = vec[i];
+		j = i-1;
+		while (j >= 0 && vec[j] > cur_value){
+			vec[j+1] = vec[j];
+			j--;
+		}
+		vec[j+1] = cur_value;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	std::vector<int> prueba;
+	double Time;
+	srand(time(NULL));
+	/*for(int j = 10000; j <= 100000; j += 10000){ //random
+		prueba.clear();
+		for(int i = 0; i < j; i++){ //10k -> 100k, 10k jumps
+			prueba.push_back(rand());
+		}
+		clock_t begin = clock();
+		insert_sort(prueba);
+		clock_t end = clock();
+		//std::cout << double(begin) << ' ' << double(end) << '\n';
+		Time = double(end-begin) / CLOCKS_PER_SEC;
+		std::cout << "Time for " << j << " elements: " << Time << std::endl;
+	}*/
+	for(int j = 1000000; j <= 10000000; j += 1000000){ //ordered
+		prueba.clear();
+		for(int i = 0; i < j; i++){ //10k -> 100k, 10k jumps
+			prueba.push_back(i);
+		}
+		clock_t begin = clock();
+		insert_sort(prueba);
+		clock_t end = clock();
+		//std::cout << double(begin) << ' ' << double(end) << '\n';
+		Time = double(end-begin) / CLOCKS_PER_SEC;
+		std::cout << "Time for " << j << " elements (ordered): " << Time << std::endl;
+	}
+	/*for(int j = 10000; j <= 100000; j += 10000){ //ordered
+		prueba.clear();
+		for(int i = j; i > 0; i--){ //10k -> 100k, 10k jumps
+			prueba.push_back(i);
+		}
+		clock_t begin = clock();
+		insert_sort(prueba);
+		clock_t end = clock();
+		//std::cout << double(begin) << ' ' << double(end) << '\n';
+		Time = double(end-begin) / CLOCKS_PER_SEC;
+		std::cout << "Time for " << j << " elements (reverse ordered): " << Time << std::endl;
+	}*/
+	/*clock_t begin = clock();
+	insert_sort(prueba);
+	clock_t end = clock();
+	//std::cout << double(begin) << ' ' << double(end) << '\n';
+	Time = double(end-begin) / CLOCKS_PER_SEC;
+	std::cout << "Time: " << Time << std::endl;
+	for(int i = 0; i < prueba.size(); i++){
+		//std::cout << prueba.at(i) << ' ';
+	}
+	std::cout << '\n';*/
+	return 0;
+}
+
