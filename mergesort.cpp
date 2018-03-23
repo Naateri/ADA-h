aa54b741d49950df1aab8d7291d759b1841b54b8
Renato Postigo <renato.postigo@ucsp.edu.pe>
2018-03-23 16:21:58 -0500

    merge en menos líneas

--- mergesort.cpp
+++ mergesort.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <algorithm>
+#include <iterator>
 #include <fstream>
 //using namespace std;
 
@@ -26,13 +27,8 @@ int_vec merge(int_vec A, int_vec B){
 			B.erase(B.begin());
 		}
 	}
-	for(int i = 0; i < A.size(); i++){
-		result.push_back(A.at(i));
-	}
-	//std::copy(A.begin(), A.begin() + A.size(), result.end());
-	for(int i = 0; i < B.size(); i++){
-		result.push_back(B.at(i));
-	}
+	std::copy(std::begin(A), std::end(A), std::back_inserter(result));
+	std::copy(std::begin(B), std::end(B), std::back_inserter(result));
 	return result;
 }
 
@@ -40,20 +36,15 @@ int_vec mergesort(int_vec& A){
 	if (A.size() <= 1)
 		return A;
 	int_vec left, right, result;
-	int i;
-	for(i = 0; i < A.size()/2; i++){
-		left.push_back(A[i]);
-	}
-	for(i = A.size()/2; i < A.size(); i++){
-		right.push_back(A[i]);
-	}
+	std::copy(std::begin(A), std::begin(A) + A.size()/2, std::back_inserter(left));
+	std::copy(std::begin(A) + A.size()/2, std::end(A), std::back_inserter(right));
 	result = merge(mergesort(left), mergesort(right));
 	A = result;
 	return result;
 }
 
 int main(int argc, char *argv[]) {
-	int_vec prueba;/* = {10,9,8,7,6,5,4,3,2,1};*/
+	int_vec prueba; /*= {10,9,8,7,6,5,4,3,2,1};*/
 	double Time;
 	srand(time(NULL));
 	/*std::cout << "Prior to mergesort:\n";
@@ -63,42 +54,30 @@ int main(int argc, char *argv[]) {
 	print_vec(prueba);*/
 	std::ofstream file;
 	file.open("m_random.txt");
-	/*std::ofstream file2;
+	std::ofstream file2;
 	file2.open("m_ordered.txt");
 	std::ofstream file3;
-	file3.open("m_orderedB.txt");*/
+	file3.open("m_orderedB.txt");
 	for(int j = 100000; j <= 1000000; j += 100000){ //random
-		int a;
 		prueba.clear();
 		for(int i = 0; i < j; i++){ //10k -> 100k, 10k jumps
 			prueba.push_back(rand());
 		}
-		//print_vec(prueba);
-		//std::cin >> a;
 		clock_t begin = clock();
 		mergesort(prueba);
 		clock_t end = clock();
-		//print_vec(prueba);
-		//std::cin >> a;
-		//std::cout << double(begin) << ' ' << double(end) << '\n';
 		Time = double(end-begin) / CLOCKS_PER_SEC;
 		//std::cout << "Time for " << j << " elements: " << Time << std::endl;
 		file << j << ' ' << Time << std::endl;
 	}
-	/*for(int j = 100000; j <= 1000000; j += 100000){ //ordered
-		int a;
+	for(int j = 100000; j <= 1000000; j += 100000){ //ordered
 		prueba.clear();
 		for(int i = 0; i < j; i++){ //10k -> 100k, 10k jumps
 			prueba.push_back(i);
 		}
-		//print_vec(prueba);
-		//std::cin >> a;
 		clock_t begin = clock();
 		mergesort(prueba);
 		clock_t end = clock();
-		//print_vec(prueba);
-		//std::cin >> a;
-		//std::cout << double(begin) << ' ' << double(end) << '\n';
 		Time = double(end-begin) / CLOCKS_PER_SEC;
 		//std::cout << "Time for " << j << " elements: " << Time << std::endl;
 		file2 << j << ' ' << Time << std::endl;
@@ -115,10 +94,9 @@ int main(int argc, char *argv[]) {
 		Time = double(end-begin) / CLOCKS_PER_SEC;
 		//std::cout << "Time for " << j << " elements: " << Time << std::endl;
 		file3 << j << ' ' << Time << std::endl;
-	}*/
+	}
 	file.close();
-	/*file2.close();
-	file3.close();*/
+	file2.close();
+	file3.close();
 	return 0;
 }
-
