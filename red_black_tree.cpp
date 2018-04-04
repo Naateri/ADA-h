638ad45af7f71bf88b73db62ac7221d43495f734
Nateri <renato.postigo@ucsp.edu.pe>
2018-04-03 21:10:37 -0500

    All of the content of the slides is programmed. Slides didn't cover all cases. Gg no re xdddd

--- red_black_tree.cpp
+++ red_black_tree.cpp
@@ -20,6 +20,7 @@ class RedBlack{
 	node_list path;
 	void recolor(RBNode* v);
 	void right_rotate(RBNode** p);
+	void left_rotate(RBNode** p);
 public:
 	RBNode* root;
 	RedBlack();
@@ -28,10 +29,22 @@ public:
 	bool remove(int x);
 	void inorder(RBNode* p);
 	RBNode** Rep(RBNode** p);
+	void printTree(RBNode* p);
 };
 
 RedBlack::RedBlack(){
 	this->root = 0;
+}
+
+void RedBlack::printTree(RBNode* p){
+	if (!p) return;
+	std::cout << "Valor: " << p->m_x << " Color: ";
+	if (p->colour) std::cout << "Negro\n"; 
+	else std::cout << "Rojo\n";
+	if (p->m_nodes[0]) std::cout << "Hijo izquierdo: " << p->m_nodes[0]->m_x << ' ';
+	if (p->m_nodes[1]) std::cout << "Hijo derecho: " << p->m_nodes[1]->m_x << ' ';
+	printTree(p->m_nodes[0]);
+	printTree(p->m_nodes[1]);
 }
 
 bool RedBlack::find(int x, RBNode**& p){
@@ -39,26 +52,29 @@ bool RedBlack::find(int x, RBNode**& p){
 	for(p = &root; *p && (*p)->m_x != x; p = &( (*p)->m_nodes[ (*p)->m_x < x ] ) ){
 		path.push_back(*p);
 	}
-	return ( (*p)->m_x == x );
+	return ( (*p) && (*p)->m_x == x );
 }
 
 bool RedBlack::insert(int x){
 	RBNode** v;
 	if (find (x, v) ) return 0;
 	*v = new RBNode(x);
-	//fix things if necessary -> recolor
+	recolor(*v);
 }
 
 void RedBlack::recolor(RBNode* v){
+	if ( v == root){
+		root->colour = 1;
+		return;
+	}
 	RBNode* p, *g_parent, *uncle;
 	node_list::iterator it = path.end();
-	p = *(--it);
-	path.pop_back();
-	if (parent->colour) return;
-	g_parent = *(--it);
-	path.pop_back();
+	--it;
+	p = *(it--);
+	if (p->colour) return;
+	g_parent = *(it);
 	uncle = g_parent->m_nodes[1];
-	if (! uncle->colour){
+	if (uncle && !uncle->colour){
 		p->colour = 1;
 		uncle->colour = 1;
 		g_parent->colour = 0;
@@ -67,11 +83,62 @@ void RedBlack::recolor(RBNode* v){
 		p->colour = 1;
 		g_parent->colour = 0;
 		right_rotate(&g_parent);
+		//left_rotate(&g_parent);
 	}
 }
 
+void RedBlack::right_rotate(RBNode** p){
+	RBNode* father, *gfather, *temp, *bl;
+	node_list::iterator it = path.end();
+	while(*p != *(it) ){
+		it--;
+	}
+	gfather = *p;
+	father = gfather->m_nodes[1];
+	if ( (*it) != this->root ) {
+		it--;
+		temp = *(it);
+	}
+	bl = father->m_nodes[0];
+	father->m_nodes[0] = gfather;
+	gfather->m_nodes[1] = bl;
+	if (root == gfather) root = father;
+	else {
+		if (father->m_x < temp->m_x ) temp->m_nodes[0] = father;
+		else temp->m_nodes[1] = father;
+	}
+}
+
+void RedBlack::left_rotate(RBNode** p){
+	RBNode* father, *gfather, *temp, *bl;
+	node_list::iterator it = path.end();
+	while(*p != *(it) ){
+		it--;
+	}
+	gfather = *p;
+	father = gfather->m_nodes[0];
+	if ( (*it) != this->root ) {
+		it--;
+		temp = *(it);
+	}
+	bl = father->m_nodes[1];
+	father->m_nodes[1] = gfather;
+	gfather->m_nodes[0] = bl;
+	if (root == gfather) root = father;
+	else {
+		if (father->m_x < temp->m_x ) temp->m_nodes[0] = father;
+		else temp->m_nodes[1] = father;
+	}
+}
+
+
 int main(int argc, char *argv[]) {
-	
+	RedBlack Example;
+	Example.insert(5);
+	Example.insert(3);
+	Example.insert(6);
+	Example.insert(1);
+	Example.printTree(Example.root);
 	return 0;
 }
 
